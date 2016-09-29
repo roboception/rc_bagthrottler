9f5448bfabf376462c00e8a140febade9f55002f
emmerich <christian.emmerich@roboception.de>
2016-09-29 19:02:34 +0200

    added throttling mechanism for synchronizers

--- bagthrottler/src/test_throttler.cc
+++ bagthrottler/src/test_throttler.cc
@@ -1,12 +1,19 @@
 #include <ros/ros.h>
 #include "BagThrottler.h"
 
+#include <sensor_msgs/JointState.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/sync_policies/exact_time.h>
+
 using namespace sensor_msgs;
 using namespace rc;
 using namespace std;
 
 void fastCallback(JointStateConstPtr msg)
-{ static int lastSeq = -1;
+{
+  static int lastSeq = -1;
   if (lastSeq>0 && lastSeq+1!=msg->header.seq) {
     throw runtime_error("!!!fastCallback missed event!!!");
   }
@@ -15,8 +22,10 @@ void fastCallback(JointStateConstPtr msg)
        msg->header.stamp << " seq: " << msg->header.seq);
 }
 
+ros::Publisher pub;
 void slowCallback(JointStateConstPtr msg)
-{ static int lastSeq = -1;
+{
+  static int lastSeq = -1;
   if (lastSeq>0 && lastSeq+1!=msg->header.seq) {
     throw runtime_error("!!!slowCallback missed event!!!");
   }
@@ -24,24 +33,50 @@ void slowCallback(JointStateConstPtr msg)
   ROS_INFO_STREAM("slowCallback with heavy computation, stamp: " <<
        msg->header.stamp << " seq: " << msg->header.seq);
   sleep(1);
+
+  JointStatePtr outmsg(new JointState(*msg));
+  pub.publish(outmsg);
+}
+
+void syncCallback(JointStateConstPtr msg)
+{
+  ROS_INFO_STREAM("syncCallback with heavy computation, stamp: " <<
+                                                                 msg->header.stamp << " seq: " << msg->header.seq);
+  static int lastSeq = -1;
+  if (lastSeq>0 && lastSeq+1!=msg->header.seq) {
+    throw runtime_error("!!!syncCallback missed event!!!");
+  }
+  lastSeq = msg->header.seq;
+  sleep(1);
+
+  JointStatePtr outmsg(new JointState(*msg));
+//  pub.publish(outmsg);
 }
 
 /*******************************************************************************
 *******************************************************************************/
 
+using namespace message_filters;
 int main(int argc, char *argv[])
 {
   // first we need to initialize ros node
-  ros::init(argc, argv, "throttler_test");
+  ros::init(argc, argv, "first_node");
 
   // second, we configure bag throttler to a specific rosbag topic
-  BagThrottler::throttle<sensor_msgs::JointState>("/iiwa/joint_states");
-  BagThrottler::throttle<sensor_msgs::JointState>("/output/from/other/node", "/rosbag/topic/to/be/throttled");
+//  BagThrottler::throttle<JointState>("/iiwa/joint_states");
 
   // finally, we can register our actual subscribers
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("/iiwa/joint_states", 5, &fastCallback);
-  ros::Subscriber sub2 = nh.subscribe("/iiwa/joint_states", 2, &slowCallback);
+//  pub = nh.advertise<JointState>("/joint_states", 1000);
+//  ros::Subscriber sub = nh.subscribe("/iiwa/joint_states", 5, &fastCallback);
+//  ros::Subscriber sub2 = nh.subscribe("/iiwa/joint_states", 2, &slowCallback);
+
+  message_filters::Subscriber<JointState> joint_sub(nh, "/iiwa/joint_states", 2);
+  message_filters::Subscriber<JointState> joint_sub2(nh, "/iiwa/joint_states", 10);
+  typedef sync_policies::ExactTime<JointState, JointState> MySyncPolicy;
+  Synchronizer<MySyncPolicy> sync(MySyncPolicy(1000), joint_sub, joint_sub2);
+  BagThrottler::throttle<JointState>(sync, "/iiwa/joint_states");
+  sync.registerCallback(boost::bind(&syncCallback, _1));
 
   ros::spin();
   return 0;
